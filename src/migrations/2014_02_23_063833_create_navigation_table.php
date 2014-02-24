fafec3238ebe13d343cb1aab1162998c80d6e3cc
Kamal Lamichhane <lckamal@hotmail.com>
2014-02-24 21:16:52 +0545

    migration added nullable

--- src/migrations/2014_02_23_063833_create_navigation_table.php
+++ src/migrations/2014_02_23_063833_create_navigation_table.php
@@ -25,13 +25,13 @@ class CreateNavigationTable extends Migration {
 			$table->unsignedInteger('parent');
 			$table->enum('link_type', array('url','uri','page'))->default('page');
 			$table->unsignedInteger('page_id');
-			$table->string('url');
-			$table->string('uri');
+			$table->string('url')->nullable();
+			$table->string('uri')->nullable();
 			$table->unsignedInteger('navigation_group_id');
 			$table->integer('position');
 			$table->enum('target', array('selected','_blank'))->default('selected');
-			$table->string('restricted_to');
-			$table->string('class', 50);
+			$table->string('restricted_to')->nullable();
+			$table->string('class', 50)->nullable();
 			$table->timestamps();
 		});
 	}
